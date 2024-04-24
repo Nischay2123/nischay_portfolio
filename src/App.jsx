59fefab5cf5ffb4ed37e8916c8e7295949e41b92
Nischay <nischaysharma04@gmail.com>
2024-04-24 19:51:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import {About} from './Components/About'
+import { Skills } from "./Components/Skills"
+import Projects from "./Components/Projects"
+import Contact from "./Components/Contact"
+import { Home } from "./Components/Home"
+import { Navbar } from "./Components/Navbar"
+
+
+
+
+
+function App() {
+
+
+  return (
+    
+      <>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      </>
+    
+  )
+}
+
+export default App
---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+import todo from '../assets/todoFromJs.png'
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:todo,
+        github:"",
+        live:"https://mytodowebapplication.netlify.app/",
+    }
+
+
+]
